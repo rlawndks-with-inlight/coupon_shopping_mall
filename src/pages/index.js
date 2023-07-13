1416838ed50ae606a18f77a812a376e09bf4cab2
rlawndks4 <rlawndks0423@naver.com>
2023-07-13 17:50:13 +0900

    Feat: 업데이트

--- src/pages/index.js
+++ src/pages/index.js
@@ -87,7 +87,7 @@ HomePage.getLayout = (page) => <MainLayout> {page} </MainLayout>;
 export async function getServerSideProps({ req }) {
 
   const domain = req.headers.host.split(':')[0];
-  const url = `${process.env.BACK_URL}/api/v1/auth/domain?dns=${domain}`;
+  const url = `${process.env.BACK_URL}/api/v1/auth/domain?dns=team.comagain.kr`;
   const res = await fetch(url);
   let dns_data = await res.json();
   return {

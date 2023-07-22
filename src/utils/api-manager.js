c0a436d4818784d92a99f3a788e927fa522d32ca
rlawndks4 <rlawndks0423@naver.com>
2023-07-22 19:22:03 +0900

    Feat: 업데이트

--- src/utils/api-manager.js
+++ src/utils/api-manager.js
@@ -54,7 +54,7 @@ export const get = async (url, params) => {
     return false;
   }
 }
-export const getCategoriesByManager = (params) => {
+export const getCategoriesByManager = (params) => { //관리자 상품 카테고리 목록출력
   const { page, page_size, s_dt, e_dt, search } = params;
   let query = {
     page, page_size, s_dt, e_dt, search
@@ -65,7 +65,7 @@ export const getCategoriesByManager = (params) => {
 
   return get(`/api/v1/manager/product-categories`, query);
 }
-export const addCategoryByManager = (params) => {
+export const addCategoryByManager = (params) => { //관리자 상품 카테고리 추가
   const { parent_id, category_type, category_name, category_description, category_file = '' } = params;
   let obj = {
     parent_id, category_type, category_name, category_description, category_file
@@ -75,23 +75,23 @@ export const addCategoryByManager = (params) => {
   }
   return post(`/api/v1/manager/product-categories`, obj);
 }
-export const updateCategoryByManager = (params) => {
+export const updateCategoryByManager = (params) => { //관리자 상품 카테고리 수정
   const { id, parent_id, category_type, category_name, category_description, category_file } = params;
   let obj = {
     parent_id, category_type, category_name, category_description, category_file
   }
   return put(`/api/v1/manager/product-categories/${id}`, obj);
 }
-export const getCategoryByManager = (params) => {
+export const getCategoryByManager = (params) => { //관리자 상품 카테고리 단일 출력
   const { id } = params;
   const response = axiosIns().get(`/api/v1/manager/product-categories/${id}`);
   console.log(response.data);
 }
-export const deleteCategoryByManager = (params) => {
+export const deleteCategoryByManager = (params) => { //관리자 상품 카테고리 삭제
   const { id } = params;
   return deleteItem(`/api/v1/manager/product-categories/${id}`);
 }
-export const getProductsByManager = (params) => {
+export const getProductsByManager = (params) => { //관리자 상품목록 출력
   const { page, page_size, s_dt, e_dt, search, category_id } = params;
   let query = {
     page, page_size, s_dt, e_dt, search, category_id
@@ -103,29 +103,29 @@ export const getProductsByManager = (params) => {
 
   return get(`/api/v1/manager/products`, query);
 }
-export const addProductByManager = (params) => {
+export const addProductByManager = (params) => { //관리자 상품 추가
   const { category_id, product_name = '', product_comment = '', product_price = 0, product_sale_price = 0, brand_name = '', origin_name = '', mfg_name = '', model_name = '', product_description = '', product_file, product_sub_file } = params;
   let obj = {
     category_id, product_name, product_comment, product_price, product_sale_price, brand_name, origin_name, mfg_name, model_name, product_description, product_file, product_sub_file
   }
   return post(`/api/v1/manager/products`, obj);
 }
-export const updateProductByManager = (params) => {
+export const updateProductByManager = (params) => { //관리자 상품 수정
   const { id, category_id, product_name = '', product_comment = '', product_price = 0, product_sale_price = 0, brand_name = '', origin_name = '', mfg_name = '', model_name = '', product_description = '', product_file, product_sub_file } = params;
   let obj = {
     category_id, product_name, product_comment, product_price, product_sale_price, brand_name, origin_name, mfg_name, model_name, product_description, product_file, product_sub_file
   }
   return put(`/api/v1/manager/products/${id}`, obj);
 }
-export const getProductByManager = (params) => {
+export const getProductByManager = (params) => { //관리자 상품 단일 출력
   const { id } = params;
   return get(`/api/v1/manager/products/${id}`);
 }
-export const deleteProductByManager = (params) => {
+export const deleteProductByManager = (params) => { //관리자 상품 삭제
   const { id } = params;
   return deleteItem(`/api/v1/manager/products/${id}`);
 }
-export const getUsersByManager = (params) => {
+export const getUsersByManager = (params) => { //관리자 유저목록 출력
   const { page, page_size, s_dt, e_dt, search } = params;
   let query = {
     page, page_size, s_dt, e_dt, search
@@ -136,28 +136,28 @@ export const getUsersByManager = (params) => {
 
   return get(`/api/v1/manager/users`, query);
 }
-export const addUserByManager = (params) => {
-  const {  } = params;
+export const addUserByManager = (params) => { //관리자 유저 추가
+  const { } = params;
   let obj = {
 
   }
   return post(`/api/v1/manager/users`, obj);
 }
-export const updateUserByManager = (params) => {
-  const {  } = params;
+export const updateUserByManager = (params) => { //관리자 유저 수정
+  const { } = params;
   let obj = {
   }
   return put(`/api/v1/manager/users/${id}`, obj);
 }
-export const getUserByManager = (params) => {
+export const getUserByManager = (params) => { //관리자 유저 단일 출력
   const { id } = params;
   return get(`/api/v1/manager/users/${id}`);
 }
-export const deleteUserByManager = (params) => {
+export const deleteUserByManager = (params) => { //관리자 유저 삭제
   const { id } = params;
   return deleteItem(`/api/v1/manager/users/${id}`);
 }
-export const getSellersByManager = (params) => {
+export const getSellersByManager = (params) => { //관리자 셀러 목록 출력
   const { page, page_size, s_dt, e_dt, search } = params;
   let query = {
     page, page_size, s_dt, e_dt, search
@@ -168,27 +168,27 @@ export const getSellersByManager = (params) => {
 
   return get(`/api/v1/manager/merchandises`, query);
 }
-export const addSellerByManager = (params) => {
-  const {  } = params;
+export const addSellerByManager = (params) => { //관리자 셀러 추가
+  const { } = params;
   let obj = {
   }
   return post(`/api/v1/manager/merchandises`, obj);
 }
-export const updateSellerByManager = (params) => {
-  const {  } = params;
+export const updateSellerByManager = (params) => { //관리자 셀러 수정
+  const { } = params;
   let obj = {
   }
   return put(`/api/v1/manager/merchandises/${id}`, obj);
 }
-export const getSellerByManager = (params) => {
+export const getSellerByManager = (params) => { //관리자 셀러 단일 출력
   const { id } = params;
   return get(`/api/v1/manager/merchandises/${id}`);
 }
-export const deleteSellerByManager = (params) => {
+export const deleteSellerByManager = (params) => { //관리자 셀러 삭제
   const { id } = params;
   return deleteItem(`/api/v1/manager/merchandises/${id}`);
 }
-export const getPostCategoriesByManager = (params) => {
+export const getPostCategoriesByManager = (params) => { //관리자 게시글 카테고리 목록 출력
   const { page, page_size, s_dt, e_dt, search } = params;
   let query = {
     page, page_size, s_dt, e_dt, search
@@ -198,23 +198,23 @@ export const getPostCategoriesByManager = (params) => {
   if (!query['search']) delete query['search'];
   return get(`/api/v1/manager/post-categories`, query);
 }
-export const addPostCategoryByManager = (params) => {
-  const {  } = params;
+export const addPostCategoryByManager = (params) => { //관리자 게시글 카테고리 추가
+  const { } = params;
   let obj = {
   }
   return post(`/api/v1/manager/post-categories`, obj);
 }
-export const updatePostCategoryByManager = (params) => {
-  const {  } = params;
+export const updatePostCategoryByManager = (params) => { //관리자 게시글 카테고리 수정
+  const { } = params;
   let obj = {
   }
   return put(`/api/v1/manager/post-categories/${id}`, obj);
 }
-export const getPostCategoryByManager = (params) => {
+export const getPostCategoryByManager = (params) => { //관리자 게시글 카테고리 단일 출력
   const { id } = params;
   return get(`/api/v1/manager/post-categories/${id}`);
 }
-export const deletePostCategoryByManager = (params) => {
+export const deletePostCategoryByManager = (params) => { //관리자 게시글 카테고리 삭제
   const { id } = params;
   return deleteItem(`/api/v1/manager/post-categories/${id}`);
 }
--- src/utils/api-shop.js
+++ src/utils/api-shop.js
@@ -1,20 +1,20 @@
 import { get, post, put, deleteItem } from './api-manager';
 
-export const getCategoriesByUser = (params) => {
+export const getCategoriesByUser = (params) => { // 유저 상품 카테고리 출력
   const { brand_id } = params;
   let query = {
     brand_id
   }
   return get(`/api/v1/shop/shop`, query);
 }
-export const getProductsByUser = (params) => {
+export const getProductsByUser = (params) => { // 유저 카테고리 기반 상품 목록 출력
   const { brand_id, category_id } = params;
   let query = {
     brand_id, category_id
   }
   return get(`/api/v1/shop/shop/category`, query);
 }
-export const getProductByUser = (params) => {
+export const getProductByUser = (params) => { // 유저 상품 단일 출력
   const { product_id } = params;
   let query = {
     product_id

e7c26d58e517d7403ec9b30d16127b00f55c0717
rlawndks4 <rlawndks0423@naver.com>
2023-08-04 17:20:32 +0900

    feat: update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -30,13 +30,35 @@ import { MotionLazyContainer } from 'src/components/animate';
 import { ModalProvider } from 'src/components/dialog/ModalProvider';
 
 const App = (props) => {
-  const { Component, pageProps } = props;
+  const { Component, pageProps, head_data } = props;
   const getLayout = Component.getLayout ?? ((page) => page);
 
+  useEffect(() => {
+    console.log(head_data)
+  }, [])
   return (
     <>
       <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <title>{head_data?.name}</title>
+        <meta
+          name='description'
+          content={head_data?.og_description}
+        />
+        <link rel='shortcut icon' href={head_data?.favicon_img} />
+        <link rel="apple-touch-icon" sizes="180x180" href={head_data?.favicon_img} />
+        <meta name='keywords' content={head_data?.name} />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={head_data?.name} />
+        <meta property="og:image" content={head_data?.og_img} />
+        <meta property="og:url" content={'https://' + head_data?.dns} />
+        <meta property="og:description" content={head_data?.og_description} />
+        <meta name="author" content="purplevery" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=5.0, user-scalable=0" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content={head_data?.name} />
+        <meta name="theme-color" content={head_data?.theme_css?.main_color} />
       </Head>
       <AuthProvider>
         <ReduxProvider store={store}>
@@ -44,13 +66,13 @@ const App = (props) => {
             <ThemeColorPresets>
               <ThemeContrast>
                 <SettingsProvider>
-                <MotionLazyContainer>
-                  <ThemeProvider>
-                    <ModalProvider>
-                     {getLayout(<Component {...pageProps} />)}
-                    </ModalProvider>
-                    <Toaster position={'right-top'} toastOptions={{ className: 'react-hot-toast' }} />
-                  </ThemeProvider>
+                  <MotionLazyContainer>
+                    <ThemeProvider>
+                      <ModalProvider>
+                        {getLayout(<Component {...pageProps} />)}
+                      </ModalProvider>
+                      <Toaster position={'right-top'} toastOptions={{ className: 'react-hot-toast' }} />
+                    </ThemeProvider>
                   </MotionLazyContainer>
                 </SettingsProvider>
               </ThemeContrast>
@@ -61,4 +83,23 @@ const App = (props) => {
     </>
   );
 }
+App.getInitialProps = async ({ ctx }) => {
+  try {
+    let head_data = {}
+    const host = ctx.req ? ctx.req.headers.host.split(':')[0] : '';
+    const url = `${process.env.BACK_URL}/api/v1/auth/domain?dns=${host}`;
+    const res = await fetch(url);
+    head_data = await res.json();
+
+    return {
+      head_data: head_data
+    }
+  } catch (err) {
+    console.log(err)
+    return {
+      head_data: {}
+    }
+  }
+
+};
 export default App

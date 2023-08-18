b19cd09d7bacd543e6387553314114b0e600cacb
rlawndks4 <rlawndks0423@naver.com>
2023-08-18 18:31:41 +0900

    feat: update

--- src/views/shop/service/article_category/id/demo-2.js
+++ src/views/shop/service/article_category/id/demo-2.js
@@ -1,14 +1,7 @@
 import { TableRow } from "evergreen-ui";
 import styled from "styled-components";
 
-const notice = (Object.values(data)).map((notice) => (
-  <TableRow key={notice.id}>
-    <TableColumn>{notice.id}</TableColumn>
-    <TableColumn>{notice.title}</TableColumn>
-    <TableColumn>{notice.createDate}</TableColumn>
-    <TableColumn>{notice.userName}</TableColumn>
-  </TableRow>
-));
+
 //공지사항, faq 등 상세 페이지 박이규
 const Demo2 = (props) => {
   const {

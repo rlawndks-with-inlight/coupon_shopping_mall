1baf2301b21021dc4ae7ff23e4253fa0eda13e2f
rladlsdnr <kimin6756@gmail.com>
2024-02-14 00:24:40 +0900

    기본정보 수정

--- src/components/settings/SettingsContext.js
+++ src/components/settings/SettingsContext.js
@@ -151,7 +151,7 @@ export function SettingsProvider({ children }) {
       dns_data['blog_obj'] = data?.blog_obj ?? [];
       dns_data['payment_modules'] = data?.payment_modules ?? [];
       dns_data['products'] = data?.products ?? [];
-      dns_data['basic_info'] = data?.products ?? [];
+      dns_data['basic_info'] = data?.basic_info ?? [];
       onChangeWishData(data?.user_wishs ?? []);
       onChangeCategoryList(data?.product_category_groups ?? []);
       onChangePropertyList(data?.product_property_groups ?? []);
